4d376fe318025b4b90a78016f78051943e985875
ramzdf <ramazdf@gmail.com>
2022-04-05 22:18:58 +0700

    rama

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
+/*rama
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
